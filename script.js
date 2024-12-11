52dae7e217abc1280deaf8413813c51e16cb808e
SteveStar <71157526+SteveStar@users.noreply.github.com>
2024-12-11 10:20:53 -0500

    Added more logs to post the difference between non child and child

--- script.js
+++ script.js
@@ -32,7 +32,7 @@ let product = new ProductProperties("Apples", 2.50, 50);
 product.getTotalValue();
 console.log(product.toString());
 
-// Part 2
+// Part 2 & 3
 class PerishableProductProperties extends ProductProperties {
     constructor(name, price, quantity, expirationDate) {
         super(name, price, quantity); 
@@ -47,12 +47,13 @@ class PerishableProductProperties extends ProductProperties {
 
 const milk = new PerishableProductProperties("Milk", 1.50, 10, "2024-12-31");
 const yogurt = new PerishableProductProperties("Yogurt", 2.00, 5, "2024-12-25");
+const orange = new ProductProperties("Orange", 2.50, 8);
 
+// this is before the discounts
+console.log("Before Discount:");
 console.log(milk.toString());
 console.log(yogurt.toString());
+console.log(bread.toString());
 
 milk.getTotalValue();
 yogurt.getTotalValue();
-
-// Part 3
-
